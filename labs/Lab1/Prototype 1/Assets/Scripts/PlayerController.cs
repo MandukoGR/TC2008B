5f90f051bbe9be8ccd9ee33176a6ca19422b58d2
MandukoGR <gurom84@outlook.com>
2022-11-04 18:08:02 -0600

    Sesion4 multiagentes y lab

--- labs/Lab1/Prototype 1/Assets/Scripts/PlayerController.cs
+++ labs/Lab1/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,15 @@ public class PlayerController : MonoBehaviour
 
     public float horizontalInput;
     public float forwardInput;
+
+    //Variables cámara
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey; //Tecla que permite cambiar entre cámaras
+
+    //Variables multijugador
+    public string inputId;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,13 +29,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      horizontalInput = Input.GetAxis("Horizontal");
-      forwardInput = Input.GetAxis("Vertical");
+      horizontalInput = Input.GetAxis("Horizontal" + inputId);
+      forwardInput = Input.GetAxis("Vertical" + inputId);
       //Mover vehiculo hacia adelante
       //transform.Translate(0,0,1);
       transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
        //Modificar el giro
       // transform.Translate(Vector3.right * Time.deltaTime* turnSpeed * horizontalInput);
       transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        //Cambio entre cámaras
+      if(Input.GetKeyDown(switchKey))
+      {
+         mainCamera.enabled = !mainCamera.enabled;
+         hoodCamera.enabled = !hoodCamera.enabled;
+      }
     }
 }
