0a35b4aba22199b9fc96220097f4eedf577d56f5
MandukoGR <gurom84@outlook.com>
2022-11-17 19:06:48 -0600

    Avance Unity

--- New Unity Project/Assets/Scripts/MoveProjectile.cs
+++ New Unity Project/Assets/Scripts/MoveProjectile.cs
@@ -4,19 +4,42 @@ using UnityEngine;
 
 public class MoveProjectile : MonoBehaviour
 {
-    public float speed = 10f;
-    public float angle = 90f;
+    public float speed = 0f;
+    public float angle = 0f;
+    private GameObject boss;
+    // get boss gameobject
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        boss = GameObject.Find("Character");
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // wait for 1 second before rotating
+        StartCoroutine(RotateProjectile());
+        // Destroy the projectile after 10 seconds and -1 to bullet count
+        StartCoroutine(destroyBullet());
+       
+        
+        // boss.GetComponent<ShootingEnemy>().bulletCount--;
+        
+    }
+    IEnumerator destroyBullet()
+    {
+        yield return new WaitForSeconds(10f);
+        Destroy(gameObject);
+        boss.GetComponent<ShootingEnemy>().bulletCount--;
+    }
+  
+    IEnumerator RotateProjectile()
+    {
+        yield return new WaitForSeconds(3f);
         transform.Rotate(Vector3.up * Time.deltaTime * angle);
         
     }
+    
 }
--- New Unity Project/Assets/Scripts/ShootingEnemy.cs
+++ New Unity Project/Assets/Scripts/ShootingEnemy.cs
@@ -1,35 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ShootingEnemy : MonoBehaviour
 {
     public GameObject projectile;
-    public float projectileSpeed = 10f;
-    public float firingRate = 0.2f;
+    private float firingRate;
+    private float projectileSpeed = 10f;
+    private float angle;
     private float delay = 0.2f;
+    public int bulletCount = 0;
+    public TextMeshProUGUI text;
 
   
 
     IEnumerator Start()
     {
-       InvokeRepeating("Fire1",delay, firingRate);
-        yield return new WaitForSeconds(3f);
+        firingRate = 2f;
+        InvokeRepeating("Fire1",delay, firingRate);
+        yield return new WaitForSeconds(10f);
         CancelInvoke("Fire1");
-        // InvokeRepeating("Fire2", delay, firingRate);
-        // yield return new WaitForSeconds(3f);
-        // CancelInvoke("Fire2");
+        yield return new WaitForSeconds(10f);
+        firingRate = 3f;
+        InvokeRepeating("Fire2", delay, firingRate);
+        yield return new WaitForSeconds(10f);
+        CancelInvoke("Fire2");
         // InvokeRepeating("Fire3", delay, firingRate);
+
+
+    }
+    void Update()
+    {
+        if (bulletCount > 0)
+        {
+            text.text = "Bullets: " + bulletCount;
+        }
+        else
+        {
+            text.text = "Bullets: 0";
+        }
+        
 
 
     }
     void Fire1()
     {
-        Vector3 startPosition = transform.position + new Vector3(0, -1, 0);
-        projectile.GetComponent<MoveProjectile>().speed = projectileSpeed;
-        Instantiate(projectile, startPosition, Quaternion.identity);
-        
+        angle = 90f;
+        GameObject boss = GameObject.Find("Character");
 
+        // Position at the middle of the ship
+        Vector3 startPosition = transform.position + new Vector3(2, 0.5f, 0);
+
+        projectile.GetComponent<MoveProjectile>().speed = projectileSpeed;
+        projectile.GetComponent<MoveProjectile>().angle = angle;
+        // Instantiate the projectile at the position and rotation of this transform
+        bulletCount++;
+        GameObject beam = Instantiate(projectile, startPosition, transform.rotation);
+        beam.transform.LookAt(boss.transform);
+        
+   
+
+        // Same thing, but for the left position
+        startPosition = transform.position + new Vector3(-2, 0.5f, 0);
+        bulletCount++;
+        beam = Instantiate(projectile, startPosition, transform.rotation);
+        beam.transform.LookAt(boss.transform);
+      
+
+
+        // Same thing, but to front position
+        startPosition = transform.position + new Vector3(0, 0.5f, 2);
+        bulletCount++;
+        beam = Instantiate(projectile, startPosition, transform.rotation);
+        beam.transform.LookAt(boss.transform);
+      
+
+        // Same thing, but to back position
+        startPosition = transform.position + new Vector3(0, 0.5f, -2);
+        bulletCount++;
+        beam = Instantiate(projectile, startPosition, transform.rotation);
+        beam.transform.LookAt(boss.transform);
+     
+
+    }
+
+    void Fire2()
+    {
+        angle = 0f;
+        GameObject boss = GameObject.Find("Character");
+
+        // generate projectiles all around the boss
+        for (int i = 0; i < 360; i += 45)
+        {
+            // Position at the middle of the ship
+            Vector3 startPosition = transform.position + new Vector3(2, 0.5f, 0);
+
+            projectile.GetComponent<MoveProjectile>().speed = projectileSpeed;
+            projectile.GetComponent<MoveProjectile>().angle = angle;
+            // Instantiate the projectile at the position and rotation of this transform
+            bulletCount++;
+            GameObject beam = Instantiate(projectile, startPosition, transform.rotation);
+            beam.transform.LookAt(boss.transform);
+            beam.transform.Rotate(0, i, 0);
+        
+        }
     }
 
 }
